71dcf17ff57dadc23224c2fcddae2a6505242b5a
Joseph Fischetti <fischettijw@gmail.com>
2020-03-23 06:16:27 -0400

    Add project files.

---
+++ FrmExtension.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace cSharpExtensionMethods
+{
+    partial class FrmExtension
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitleCase = new System.Windows.Forms.Label();
+            this.BtnExtensions = new System.Windows.Forms.Button();
+            this.TxtTitleCase = new System.Windows.Forms.TextBox();
+            this.TxtTitleCaseInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitleCase
+            // 
+            this.lblTitleCase.AutoSize = true;
+            this.lblTitleCase.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleCase.Location = new System.Drawing.Point(54, 89);
+            this.lblTitleCase.Name = "lblTitleCase";
+            this.lblTitleCase.Size = new System.Drawing.Size(88, 24);
+            this.lblTitleCase.TabIndex = 0;
+            this.lblTitleCase.Text = "TitleCase";
+            // 
+            // BtnExtensions
+            // 
+            this.BtnExtensions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExtensions.Location = new System.Drawing.Point(220, 12);
+            this.BtnExtensions.Name = "BtnExtensions";
+            this.BtnExtensions.Size = new System.Drawing.Size(206, 37);
+            this.BtnExtensions.TabIndex = 1;
+            this.BtnExtensions.Text = "Demo Extensions";
+            this.BtnExtensions.UseVisualStyleBackColor = true;
+            this.BtnExtensions.Click += new System.EventHandler(this.BtnExtensions_Click);
+            // 
+            // TxtTitleCase
+            // 
+            this.TxtTitleCase.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTitleCase.Location = new System.Drawing.Point(473, 87);
+            this.TxtTitleCase.Name = "TxtTitleCase";
+            this.TxtTitleCase.ReadOnly = true;
+            this.TxtTitleCase.Size = new System.Drawing.Size(288, 29);
+            this.TxtTitleCase.TabIndex = 2;
+            // 
+            // TxtTitleCaseInput
+            // 
+            this.TxtTitleCaseInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTitleCaseInput.Location = new System.Drawing.Point(179, 87);
+            this.TxtTitleCaseInput.Name = "TxtTitleCaseInput";
+            this.TxtTitleCaseInput.Size = new System.Drawing.Size(288, 29);
+            this.TxtTitleCaseInput.TabIndex = 3;
+            // 
+            // FrmExtension
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtTitleCaseInput);
+            this.Controls.Add(this.TxtTitleCase);
+            this.Controls.Add(this.BtnExtensions);
+            this.Controls.Add(this.lblTitleCase);
+            this.Name = "FrmExtension";
+            this.Text = "Extension Methods";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitleCase;
+        private System.Windows.Forms.Button BtnExtensions;
+        private System.Windows.Forms.TextBox TxtTitleCase;
+        private System.Windows.Forms.TextBox TxtTitleCaseInput;
+    }
+}
+
---
+++ FrmExtension.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace cSharpExtensionMethods
+{
+    public partial class FrmExtension : Form
+    {
+        public FrmExtension()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmExtension_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void BtnExtensions_Click(object sender, EventArgs e)
+        {
+            //TxtTitleCase.Text = TxtTitleCaseInput.Text.ToTitleCase();
+            TxtTitleCase.Text = "hello".AppendRandomString(5, true);
+            //TxtTitleCase.Text = TxtTitleCaseInput.Text.RandomString(10, false);
+            //MessageBox.Show("joe".IsBool().ToString());
+        }
+
+
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static string ToTitleCase(this string str)
+        {
+            return new CultureInfo("en-US", false).TextInfo.ToTitleCase(str);
+        }
+
+        public static bool IsBool(this string str)
+        {
+            if (str.ToUpper() == "TRUE" || str.ToUpper() == "FALSE")
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Hello");
+                return false;
+            }
+        }
+
+        // Generate and append a random string of a given size    
+        public enum Case
+        {
+            Upper,
+            Lower,
+            Mixed
+        }
+        public static string AppendRandomString(this string str, int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return str + builder.ToString().ToLower();
+            return str + builder.ToString();
+        }
+
+    }
+}
